5522769324095dee9cbae44e65fb1263818ac555
Jordan Svoboda <jordans2@iastate.edu>
2020-01-20 23:06:22 -0600

    Added code comments

--- lib/local_store.dart
+++ lib/local_store.dart
@@ -1,3 +1,5 @@
+//An app to demonstrate reading and writing to files on a device
+
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
@@ -10,33 +12,42 @@ void main(){
   ));
 }
 
+//Create a new stateful widget. This needs to be stateful since
+// we're updating the data used by the widget
 class LocalStorage extends StatefulWidget{
   @override
   _LocalStorageState createState() => _LocalStorageState();
 }
 
+//The state for the widget
 class _LocalStorageState extends State<LocalStorage>{
+  //The data to be printed on the screen
   String data = '';
   final myController = TextEditingController();
 
+  //Needed to cleanup the text editing controller and free
+  // its resources
   @override
   void dispose(){
     myController.dispose();
     super.dispose();
   }
 
+  // An asynchronous method to get the path
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
     // For your reference print the AppDoc directory
-    print(directory.path);
+    // print(directory.path);
     return directory.path;
   }
 
+  // An asynchronous method to open a file for reading/writing
   Future<File> get _localFile async {
     final path = await _localPath;
     return File('$path/test.txt');
   }
 
+  //A method to read from our file
   Future<String> readFromFile() async {
     try {
       final file = await _localFile;
@@ -50,6 +61,7 @@ class _LocalStorageState extends State<LocalStorage>{
     }
   }
 
+  //A method to write to our file
   Future<File> writeToFile(String text) async {
     final file = await _localFile;
     // Write the file
@@ -68,6 +80,7 @@ class _LocalStorageState extends State<LocalStorage>{
         body: Center(
           child: Column(
             children: <Widget>[
+              // The text entry field
               TextField(
                 decoration: InputDecoration(
                   border: InputBorder.none,
@@ -75,6 +88,7 @@ class _LocalStorageState extends State<LocalStorage>{
                 ),
                 controller: myController,
               ),
+              // The button that reads in the text from the field
               RaisedButton(
                   child: Text('Read Text'),
                   color: Colors.blue,
@@ -87,6 +101,7 @@ class _LocalStorageState extends State<LocalStorage>{
                     });
                   },
               ),
+              // The text being printed on the screen
               Text(
                 'The file now says:\n$data'
               ),
