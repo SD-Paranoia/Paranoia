acbbac1b9e7a62bf6dedb94589dfa8d840a3a289
Jordan Svoboda <jordans2@iastate.edu>
2020-02-04 15:07:57 -0600

    Changed the way the IV is passed from encrypting side to decrypting side. It is now just prepended to the encrypted message to create the text that is written to a file/sent to another device.

--- lib/local_store.dart
+++ lib/local_store.dart
@@ -24,7 +24,7 @@ class LocalStorage extends StatefulWidget{
 class _LocalStorageState extends State<LocalStorage>{
   //The data to be printed on the screen
   String data = '';
-  String keyVal, cipherText, lastIV;
+  String keyVal, cipherText;
   final myController = TextEditingController();
 
   //Needed to cleanup the text editing controller and free
@@ -72,21 +72,33 @@ class _LocalStorageState extends State<LocalStorage>{
     return writeFile.writeAsString(text);
   }
 
+  //A function to generate a 32 byte (256 bit) symmetric key and store it
   void generateSymmetricKey(){
     //Create a new 32 byte key and write it to a file
     writeToFile('symmetricKey.txt', CryptKey().genFortuna(32));
 
   }
 
+  //A function to encrypt a message using AES with a 256 bit key
   String encryptMsg(String key, String msg){
     AesCrypt encrypter = AesCrypt(key, 'gcm', 'pkcs7');
-    lastIV = CryptKey().genDart(16);
-    return encrypter.encrypt(msg, lastIV);
+    //Generate a new IV for the message encryption
+    String iv = CryptKey().genDart(16);
+    //Create the message to be sent by encrypting the plaintext and prepending
+    // the IV
+    String newMessage = iv + encrypter.encrypt(msg, iv);
+    return newMessage;
   }
+  //A function to decrypt a message using AES with a 256 bit key
   String decryptMsg(String key, String msg){
     try{
       AesCrypt decrypter = AesCrypt(key, 'gcm', 'pkcs7');
-      return decrypter.decrypt(msg,lastIV);
+      //Get IV from the first 16 bytes of the message
+      String iv = msg.substring(0,16);
+      //Get the encrypted text from the remaining portion of the message
+      String decryptedMessage = msg.substring(16);
+      //Decrypt the message using the IV from the message.
+      return decrypter.decrypt(decryptedMessage,iv);
     }catch(e){
       return 'Decryption error!';
     }
