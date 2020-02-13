80fe8993731eb6a52958ad0fd970240bdeef4657
Jordan Svoboda <jordans2@iastate.edu>
2020-02-13 00:53:53 -0600

    Implemented remaining functions for database interaction (insert, update, delete).

--- lib/database_functions.dart
+++ lib/database_functions.dart
@@ -9,6 +9,19 @@ class  ChatInfo{
   final String symmetricKey;
 
   ChatInfo({this.pubKey, this.name, this.symmetricKey});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'pubKey' : pubKey,
+      'name' : name,
+      'symmetricKey' : symmetricKey,
+    };
+  }
+
+  @override
+  String toString(){
+    return 'ChatInfo{pubKey: $pubKey, name: $name, symmetricKey: $symmetricKey}';
+  }
 }
 
 Future<Database> openDB() async{
@@ -26,3 +39,66 @@ Future<Database> openDB() async{
   return database;
 
 }
+
+Future<List<ChatInfo>> chats() async{
+  final Database db = await openDB();
+
+  final List<Map<String, dynamic>> maps = await db.query('chatInfo');
+
+  return List.generate(maps.length, (i) {
+    return ChatInfo(
+      pubKey: maps[i]['pubKey'],
+      name: maps[i]['name'],
+      symmetricKey: maps[i]['symmetricKey'],
+    );
+  });
+}
+
+Future<void> insertChatInfo(ChatInfo chat) async{
+  final Database db = await openDB();
+  try {
+    await db.insert(
+      'chatInfo',
+      chat.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.fail,
+    );
+  }catch (e){
+    throw Exception(e.toString());
+  }
+}
+
+Future<int> updateChatInfo(ChatInfo chat) async{
+  final Database db = await openDB();
+  int status;
+  try {
+    status = await db.update(
+      'chatInfo',
+      chat.toMap(),
+      //Ensure matching public key
+      where: "pubKey = ?",
+      //Pass the pubKey as a whereArg to prevent SQL injection
+      whereArgs: [chat.pubKey],
+      conflictAlgorithm: ConflictAlgorithm.fail,
+    );
+  }catch (e){
+    throw Exception(e.toString());
+  }
+  return status;
+}
+
+Future<int> deleteChatInfo(ChatInfo chat) async{
+  final Database db  = await openDB();
+  int status;
+  try {
+    status = await db.delete(
+      'chatInfo',
+      where: "pubKey = ?",
+      whereArgs: [chat.pubKey],
+    );
+  }catch (e){
+    throw Exception(e.toString());
+  }
+  return status;
+
+}
+
