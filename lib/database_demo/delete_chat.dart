4b925f065ebaec68af6281357041bb65c18d1a26
Jordan Svoboda <jordans2@iastate.edu>
2020-02-13 00:52:16 -0600

    delete_chat should now be functioning properly for the demo.

--- lib/database_demo/delete_chat.dart
+++ lib/database_demo/delete_chat.dart
@@ -1,6 +1,7 @@
 
 import'package:paranoia/database_functions.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DeleteChatDemo extends StatefulWidget{
   @override
@@ -8,16 +9,72 @@ class DeleteChatDemo extends StatefulWidget{
 }
 
 class _DeleteChatState extends State<DeleteChatDemo>{
+  ChatInfo newChat, chat;
+  String success;
+  final myController = TextEditingController();
 
+  ChatInfo createNewChat(String id){
+    return ChatInfo(
+      pubKey: id,
+      name: "User_" + id,
+      symmetricKey: null,
+    );
+  }
+
+  //Needed to cleanup the text editing controller and free
+  // its resources
+  @override
+  void dispose(){
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState(){
     super.initState();
+    success = '';
   }
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(title: Text("Delete Chat from Database")),
+        body: Center(
+            child: Column(
+                children: <Widget>[
+                  // The text entry field
+                  TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter a unique ID (A Public Key perhaps)'
+                    ),
+                    controller: myController,
+                  ),
+                  RaisedButton(
+                      child: Text("Delete Chat"),
+                      onPressed: () {
+                        newChat = createNewChat(myController.text);
+                        deleteChatInfo(newChat).then((int retVal) {
+                          if(retVal > 0) {
+                            setState(() {
+                              chat = newChat;
+                              success = "Deleted Chat: $chat";
+                            });
+                          }else{
+                            setState(() {
+                              success = "Nothing to delete!";
+                            });
+                          }
+                        }, onError: (e){
+                          setState(() {
+                            success = "Failed to delete chat: $e";
+                          });
+                        });
+                      }),
+                  Text(success)
+                ]
+            )
+        )
+    );
   }
 }
