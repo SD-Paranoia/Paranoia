60a038b721bfec8ea24a583e90f8b52f5f2f4680
Andre C <andrec@iastate.edu>
2020-04-18 14:32:01 -0500

    Networking API

--- lib/networking.dart
+++ lib/networking.dart
@@ -2,18 +2,18 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-Future<http.Response> sendMsg(String msg, String fingerPrint, String ipPort) async{
+Future<http.Response> sendMsg(String msg, String fingerPrint, String signedChallenge, String groupID, String ipPort) async{
   var client = http.Client();
    http.Response uriResponse;
   try {
-    uriResponse = await client.post(ipPort,
+    uriResponse = await client.post(ipPort+"/write",
       headers: {"Content-Type": "application/json"},
-      body: json.encode({'Message': msg, 'Recipient': fingerPrint})
+      body: json.encode({"FingerPrint": fingerPrint, "SignedChallenge":signedChallenge,"GroupID":groupID,"Content":msg})
     );
 
     if (uriResponse.statusCode == 200){
       //Potentially do something else here
-      //print("Message sent successfully!");
+      print("Message sent successfully!");
 
     }
     else{
@@ -26,18 +26,18 @@ Future<http.Response> sendMsg(String msg, String fingerPrint, String ipPort) asy
   return uriResponse;
 }
 
-Future<http.Response> registerUser(String pubKey, String pubkey2, String ipPort) async {
+Future<http.Response> registerUser(String pubKey, String signature, String ipPort) async {
   var client = http.Client();
   http.Response uriResponse;
 
   try {
-    uriResponse = await client.post(ipPort,
+    uriResponse = await client.post(ipPort+"/reg",
         headers: {"Content-Type": "application/json"},
-        body: json.encode({'Pubkey1': pubKey, 'Pubkey2': pubkey2})
+        body: json.encode({'Public': pubKey, 'Signature': signature})
     );
 
     if (uriResponse.statusCode == 200){
-      //TODO -- do something here
+      print("User registered!");
     }
     else {
       print("Failed to register chat");
@@ -49,3 +49,77 @@ Future<http.Response> registerUser(String pubKey, String pubkey2, String ipPort)
   return uriResponse;
 }
 
+Future<http.Response> challengeUser(String fingerPrint, String ipPort) async {
+  var client = http.Client();
+  http.Response uriResponse;
+
+  try {
+    uriResponse = await client.post(ipPort+"/chal",
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({'Fingerprint': fingerPrint})
+    );
+
+    if (uriResponse.statusCode == 200){
+      //TODO -- do something here
+      String uuid = uriResponse.toString();
+      print(uuid);
+    }
+    else {
+      print("Failed challenge stage");
+    }
+  }
+  finally{
+    client.close();
+  }
+  return uriResponse;
+}
+
+Future<http.Response> createGroup(String UUID, String members, String fingerPrint, String signedChallenge, String ipPort) async {
+  var client = http.Client();
+  http.Response uriResponse;
+
+  try {
+    uriResponse = await client.post(ipPort+"/convo",
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({'UUID': UUID, "Members":[members],"FingerPrint":fingerPrint, "SignedChallenge":signedChallenge})
+    );
+
+    if (uriResponse.statusCode == 200){
+      //TODO -- do something here
+      String response = uriResponse.toString();
+      print(response);
+    }
+    else {
+      print("Failed to create group");
+    }
+  }
+  finally{
+    client.close();
+  }
+  return uriResponse;
+}
+
+Future<http.Response> getMsg(String fingerPrint, String signedChallenge, String ipPort) async {
+  var client = http.Client();
+  http.Response uriResponse;
+
+  try {
+    uriResponse = await client.post(ipPort+"/read",
+        headers: {"Content-Type": "application/json"},
+        body: json.encode({'Fingerprint': fingerPrint,"SignedChallenge":signedChallenge})
+    );
+
+    if (uriResponse.statusCode == 200){
+      //TODO -- do something here
+      String response = uriResponse.toString();
+      print(response);
+    }
+    else {
+      print("Failed to read message");
+    }
+  }
+  finally{
+    client.close();
+  }
+  return uriResponse;
+}
