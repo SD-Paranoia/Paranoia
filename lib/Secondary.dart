890311783c7161cf40b32565af07f8fd0861c5bf
TheToddLuci0 <woolery@iastate.edu>
2020-04-25 01:05:07 -0500

    This may do it

--- lib/Secondary.dart
+++ lib/Secondary.dart
@@ -28,25 +28,11 @@ class Secondary extends StatefulWidget {
 class _SecondaryState extends State<Secondary> {
   String keyVal;
 
-  final myController = TextEditingController();
-  final name = TextEditingController();
   final semkey = TextEditingController();
 
   @override
   void dispose(){
-    myController.dispose();
     qrcontroller?.dispose();
-    super.dispose();
-  }
-
-  @override
-  void dispose1(){
-    name.dispose();
-    super.dispose();
-  }
-
-  @override
-  void dispose2(){
     semkey.dispose();
     super.dispose();
   }
@@ -84,62 +70,12 @@ class _SecondaryState extends State<Secondary> {
                       qrcontroller.flipCamera();
                     },
                   ),
-                  Text("Enter Server Information"),
-                  TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'URI input (IP:PORT)'
-                    ),
-                    controller: myController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Name of User (optional)'
-                    ),
-                    controller: name,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Symmetric key for encryption (stubbed for TODO)'
-                    ),
-                    controller: semkey,
-                  ),
                   RaisedButton(
-                    child: Text("Save Info"),
+                    child: Text("Next"),
                     color: Colors.greenAccent[400],
                     onPressed: (){
-                      //TODO -- register user via network
-                      String pubKey = "";
-                      //Generate new asymmetric key and store in database
-                      generatePublicPrivateKeypair();
-
-                      //Pull current user's pubkey from database
-                      publicKeyAsString().then((String retVal) {
-                        //Get the public key
-                        pubKey = retVal;
-                        //Get the private key
-                        getPrivateKey().then((RSAPrivateKey privKey) {
-                          //Sign the public key with the private
-                          String signedPublic = rsaSign(privKey, pubKey);
-                          //Get the public key
-                          getPublicKey().then((RSAPublicKey key2) {
-                            //Now after signed, send it to server to register user
-                            registerUser(pubKey, signedPublic, myController.text);
-                          });
-                        });
-
-                        //Store chat data into database
-                        ChatInfo chat = ChatInfo (pubKey: pubKey, name: name.text, symmetricKey: semkey.text, serverAddress: myController.text);
-                        insertChatInfo(chat);
-                      });
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Group_Creation_Second(myController.text)),
-                      );
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => DataCollector(text: qrText)));
                     },
-
                   )
 
                 ])));
@@ -152,5 +88,78 @@ class _SecondaryState extends State<Secondary> {
         semkey.text = qrText;
       });
     });
+  }
+}
+
+class DataCollector extends StatelessWidget{
+
+  final String text;
+  final myController = TextEditingController();
+  final name = TextEditingController();
+
+  DataCollector({@required this.text});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Secondary User Data Collection")),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+
+            Text("Enter Server Information"),
+            TextField(
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'URI input (IP:PORT)'
+              ),
+              controller: myController,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Name of User (optional)'
+              ),
+              controller: name,
+            ),
+            RaisedButton(
+              child: Text("Save Info"),
+              color: Colors.greenAccent[400],
+              onPressed: (){
+                //TODO -- register user via network
+                String pubKey = "";
+                //Generate new asymmetric key and store in database
+                generatePublicPrivateKeypair();
+
+                //Pull current user's pubkey from database
+                publicKeyAsString().then((String retVal) {
+                  //Get the public key
+                  pubKey = retVal;
+                  //Get the private key
+                  getPrivateKey().then((RSAPrivateKey privKey) {
+                    //Sign the public key with the private
+                    String signedPublic = rsaSign(privKey, pubKey);
+                    //Get the public key
+                    getPublicKey().then((RSAPublicKey key2) {
+                      //Now after signed, send it to server to register user
+                      registerUser(pubKey, signedPublic, myController.text);
+                    });
+                  });
+
+                  //Store chat data into database
+                  ChatInfo chat = ChatInfo (pubKey: pubKey, name: name.text, symmetricKey: text, serverAddress: myController.text);
+                  insertChatInfo(chat);
+                });
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Group_Creation_Second(myController.text)),
+                );
+              },
+
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
