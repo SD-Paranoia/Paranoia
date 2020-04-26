cb58f96c46d9790cdc6d3625331fb241b383a245
Jack Potter <jpotter@iastate.edu>
2020-04-25 19:47:20 -0500

    Revert "QR Code Generation"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:paranoia/demo.dart';
+import 'package:paranoia/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
